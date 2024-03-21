85a45ad68345396e45c3ac3851d4c1a47719a39c
Администратор <asdasad@gmail.com>
2024-03-21 17:28:54 +0300

    new_user

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('hello')
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+
+
+app =  FastAPI()
+
+class NewUser(BaseModel):
+    nick_name: str
+    age: int
+    GPS: int | None = None
+
+task = []
+
+
+
+
+
+@app.get('/user/')
+def create_user(new: Annotated[NewUser, Depends()]):
+    task.append({'nick': new.nick_name,'age': new.age, 'gps': new.GPS})
+    return 'Пользователь зарегистрирован'
+@app.post('/user/')
+async def user():
+    return task
